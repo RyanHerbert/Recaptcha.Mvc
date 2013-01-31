2a00f18810223beda3da7c5369e9841ce86dda9a
Ryan Herbert <ryan.herbert@gmail.com>
2013-01-31 17:31:23 -0600

    Initial Commit

---
+++ Recaptcha.Mvc/HtmlHelperExtensions/RecaptchaHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.Mvc;
+using System.IO;
+using System.Configuration;
+using System.Web;
+
+namespace Recaptcha.Mvc.HtmlHelperExtensions
+{
+    public static class RecaptchaHelper
+    {
+        public static IHtmlString GenerateCaptcha(this HtmlHelper helper, string id = "recaptcha", string theme = "blackglass")
+        {
+            var publicKey = ConfigurationManager.AppSettings["recaptcha.publicKey"];
+            var privateKey = ConfigurationManager.AppSettings["recaptcha.privateKey"];
+
+            if (String.IsNullOrEmpty(publicKey))
+                throw new ConfigurationErrorsException("The key 'recaptcha.publicKey' must be defined in your config file");
+
+            if (String.IsNullOrEmpty(privateKey))
+                throw new ConfigurationErrorsException("The key 'recaptcha.privateKey' must be defined in your config file");
+
+            var captchaControl = new Recaptcha.RecaptchaControl
+            {
+                ID = id,
+                Theme = theme,
+                PublicKey = publicKey,
+                PrivateKey = privateKey
+            };
+
+            var htmlWriter = new HtmlTextWriter(new StringWriter());
+
+            captchaControl.RenderControl(htmlWriter);
+
+            return helper.Raw(htmlWriter.InnerWriter.ToString());
+        }
+    }
+}
---
+++ Recaptcha.Mvc/ValidateCaptchaActionFilterAttribute.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace Recaptcha.Mvc
+{
+    public class ValidateCaptchaActionFilterAttribute : ActionFilterAttribute
+    {
+        private const string ChallengeFieldKey = "recaptcha_challenge_field";
+        private const string ResponseFieldKey = "recaptcha_response_field";
+
+        private readonly string _resultKey ;
+        private readonly bool _setModelStateError ;
+        private readonly string _errorMessage ;
+
+        /// <summary>
+        /// This attribute will validate a recaptcha response OnActionExecuting and add the result to the ActionParameters and ModelState
+        /// </summary>
+        /// <param name="resultKey">The name of the ActionParameter and ModelState key to set</param>
+        /// <param name="setModelStateError">Set to false to disable setting a modelstate error</param>
+        /// <param name="errorMessage">The error message to set in the model state if setModelStateError is true</param>
+        public ValidateCaptchaActionFilterAttribute(string resultKey = "captchaIsValid", bool setModelStateError = true, string errorMessage = "Your response did not match the captcha.")
+        {
+            _resultKey = resultKey;
+            _setModelStateError = setModelStateError;
+            _errorMessage = errorMessage;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var privateKey = ConfigurationManager.AppSettings["recaptcha.privateKey"];
+
+            if (String.IsNullOrEmpty(privateKey))
+                throw new ConfigurationErrorsException("The key 'recaptcha.privateKey' must be defined in your config file");
+
+            var captchaValidator = new Recaptcha.RecaptchaValidator
+            {
+                PrivateKey = privateKey,
+                RemoteIP = filterContext.HttpContext.Request.UserHostAddress,
+                Challenge = filterContext.HttpContext.Request.Form[ChallengeFieldKey] ?? String.Empty,
+                Response = filterContext.HttpContext.Request.Form[ResponseFieldKey] ?? String.Empty
+            };
+
+            var captchaResponse = captchaValidator.Validate();
+
+            // Push the result value into a parameter in our Action
+            filterContext.ActionParameters[_resultKey] = captchaResponse.IsValid;
+
+            // Set the Model State error
+            if (!captchaResponse.IsValid && _setModelStateError)
+            {
+                filterContext.Controller.ViewData.ModelState.AddModelError(_resultKey, _errorMessage);
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
